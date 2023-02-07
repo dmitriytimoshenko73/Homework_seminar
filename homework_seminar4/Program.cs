61822b1694e4f04a2438f49661163fc10d7e5a00
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-07 19:02:17 +0300

    added task2

--- homework_seminar4/Program.cs
+++ homework_seminar4/Program.cs
@@ -42,7 +42,7 @@ SumNumbers(number, len);
 int ReadInt(string message)
 {
     Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    return int.Parse(Console.ReadLine());
 }
 
 // Функция подсчета цифр в числе

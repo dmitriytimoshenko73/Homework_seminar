2c66528cc71e0bc67906be65e77b2c6b17dcfab6
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-07 19:00:42 +0300

    added task2

--- homework_seminar4/Program.cs
+++ homework_seminar4/Program.cs
@@ -4,7 +4,7 @@
 2, 4 -> 16
 */
 
-int numberA = ReadInt("Введите число A: ");
+/*int numberA = ReadInt("Введите число A: ");
 int numberB = ReadInt("Введите число B: ");
 ToDegree(numberA, numberB);
 
@@ -26,3 +26,48 @@ int ReadInt(string message)
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine());
 }
+*/
+/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+int number = ReadInt("Введите число : ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+
+

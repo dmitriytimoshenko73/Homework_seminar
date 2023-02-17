638c5d2008532569d053cfcb2db34c400191bf72
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-17 20:40:00 +0300

    added task50

--- homework_seminar7/Program.cs
+++ homework_seminar7/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите количество столбцов ");
+﻿/*Console.Write("Введите количество столбцов ");
 int rows = int.Parse(Console.ReadLine());
 Console.Write("Введите количество строк ");
 int columns = int.Parse(Console.ReadLine());
@@ -33,3 +33,63 @@ void Print(double [ , ] matrix)
     }
 }
 
+*/
+
+
+
+Console.Write("Введите количество столбцов ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк ");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] numbers = new int[rows, colums];
+
+GetArray(numbers);
+
+Print(numbers);
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int l = 0; l < array.GetLength(1); l++)
+        {
+            array[i, l] = new Random().Next(1, 20);
+        }
+    }
+
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int l = 0; l < array.GetLength(1); l++)
+        {
+            System.Console.Write(array[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int i = ReadInt ("Введите индекс строки  ");
+int l = ReadInt ("Введите индекс столбца  ");
+
+if (i < numbers.GetLength(0) && l < numbers.GetLength(1))
+
+{
+    Console.Write(numbers[i, l]);
+}
+
+else
+
+{
+
+    Console.WriteLine($"такого числа в масссиве нет");
+
+}
+int ReadInt (string message) 
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}  

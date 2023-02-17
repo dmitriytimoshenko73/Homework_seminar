a71bb262f258662ce5759c005a1f5c004c24bc37
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-18 02:01:37 +0300

    added task52

--- homework_seminar7/Program.cs
+++ homework_seminar7/Program.cs
@@ -33,20 +33,12 @@ void Print(double [ , ] matrix)
     }
 }
 
-*/
+Console.WriteLine();
+int[,] numbersA = new int[rows, columns];
 
+GetArray(numbersA);
 
-
-Console.Write("Введите количество столбцов ");
-int rows = int.Parse(Console.ReadLine());
-Console.Write("Введите количество строк ");
-int colums = int.Parse(Console.ReadLine());
-
-int[,] numbers = new int[rows, colums];
-
-GetArray(numbers);
-
-Print(numbers);
+Prints(numbersA);
 
 void GetArray(int[,] array)
 {
@@ -60,7 +52,7 @@ void GetArray(int[,] array)
 
 }
 
-void Print(int[,] array)
+void Prints(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -75,21 +67,78 @@ void Print(int[,] array)
 int i = ReadInt ("Введите индекс строки  ");
 int l = ReadInt ("Введите индекс столбца  ");
 
-if (i < numbers.GetLength(0) && l < numbers.GetLength(1))
+if (i < numbersA.GetLength(0) && l < numbersA.GetLength(1))
 
 {
-    Console.Write(numbers[i, l]);
+    Console.Write(numbersA[i, l]);
 }
 
 else
 
 {
-
     Console.WriteLine($"такого числа в масссиве нет");
-
 }
 int ReadInt (string message) 
 {
     Console.Write(message);
     return int.Parse(Console.ReadLine());
 }  
+*/
+
+
+Console.Write("Введите количество столбцов ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк ");
+int columns = int.Parse(Console.ReadLine());
+int[,] numbers = new int[rows, columns];
+
+GetArray(numbers);
+
+Prints(numbers);
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int l = 0; l < array.GetLength(1); l++)
+        {
+            array[i, l] = new Random().Next(1, 20);
+        }
+    }
+
+}
+
+void Prints(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int l = 0; l < array.GetLength(1); l++)
+        {
+            System.Console.Write(array[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+double [] avgNumbers = new double [numbers.GetLength(1)];
+double result = 0.0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+        for (int l = 0; l < numbers.GetLength(1); l++);
+        }
+        result += numbers [i, l];
+        {
+         avgNumbers[i] = result / numbers.GetLength (0); 
+        }
+PrintAvg (avgNumbers);       
+
+void PrintAvg (double [] numbers)
+{
+    for (int l = 0; l < numbers.Length; l++ )
+    {
+        Console.Write($"Среднее арифметическое элементов в каждом столбце {result}" + " ");
+    }
+    Console.WriteLine();
+} 

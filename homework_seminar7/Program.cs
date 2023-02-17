74a959daffae3e08648823232c12976a97f70595
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-17 18:59:15 +0300

    added task47

---
+++ homework_seminar7/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите количество столбцов ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк ");
+int columns = int.Parse(Console.ReadLine());
+
+double [ , ] numbers = new double [rows , columns];
+
+GetMatrix (numbers);
+
+Print (numbers);
+
+void GetMatrix(double [ , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength (0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            matrix[i, l] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+    
+}
+
+void Print(double [ , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+

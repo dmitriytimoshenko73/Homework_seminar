cf1133abdbdddd949dc6e6113a863d444538f114
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-02-27 00:27:58 +0300

    added changes

--- homework_seminar8/Program.cs
+++ homework_seminar8/Program.cs
@@ -100,9 +100,9 @@ Console.WriteLine();
 элементов: 1 строка */
 
 Console.WriteLine("Введите размер прямоугольного массива c x d и диапазон случайных значений:");
-int c = InputNumbers("Введите c: ");
-int d = InputNumbers("Введите d: ");
-int rand = InputNumbers("Введите диапазон: от 1 до ");
+int c = InputNumbers("Введите количество строк: ");
+int d = InputNumbers("Введите количество столбцов: ");
+int rand = InputNumbers("Введите диапазон чисел: от 1 до ");
 int[,] number = new int[c, d];
 Console.WriteLine();
 GetMatrix(number);
@@ -214,23 +214,22 @@ void CreateArray(int[,] array) //Функция создания матриц
 26(1,0,1) 55(1,1,1)
 */
 
-
-int[,,] array3D = new int[2, 2, 2];
-FillArray(array3D);
-PrintIndex(array3D);
+int[,,] matrix3D = new int[2, 2, 2];
+FillArray(matrix3D);
+PrintIndex(matrix3D); 
 
 
 // Функция вывода индекса элементов 3D массива
 void PrintIndex(int[,,] arr)
 {
-    for (int i = 0; i < array3D.GetLength(0); i++)
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
     {
-        for (int j = 0; j < array3D.GetLength(1); j++)
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
         {
             Console.WriteLine();
-            for (int k = 0; k < array3D.GetLength(2); k++)
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
             {
-                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
             }
         }
     }
@@ -252,3 +251,71 @@ void FillArray(int[,,] arr)
         }
     }
 }
+
+
+// Вариант 2 
+Console.WriteLine();
+Console.WriteLine($"Введите размер массива X x Y x Z: ");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+
+int[,,] array3D = new int[x, y, z];
+CreateArray3D(array3D);
+WriteArray(array3D);
+
+
+void WriteArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.WriteLine (); 
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray3D(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 99);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 99);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
